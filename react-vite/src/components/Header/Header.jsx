9f2fa0e47806a66e11e53e67f34034e011663aee
Tayvor <tabeck98@gmail.com>
2024-08-09 17:08:25 -0600

    Header component css update.

--- react-vite/src/components/Header/Header.jsx
+++ react-vite/src/components/Header/Header.jsx
@@ -3,6 +3,12 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <div>hello from header</div>
+    <div className='header'>
+      <button className="menuBtn btn">Menu</button>
+
+      <h1 className='title'>cardBoard</h1>
+
+      <button className='loginBtn btn'>Login</button>
+    </div>
   )
 }
